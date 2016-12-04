419bcb18dbb82ac8370d7ea253b94a60406a8302
Adely Albuquerque <adely.albuquerque@gmail.com>
2016-12-04 20:24:20 -0300

    Refactor the code using zip to iterate

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -3,7 +3,7 @@ def distance(sequencia_1, sequencia_2):
     contador = 0
     if len(sequencia_1) != len(sequencia_2):
         raise ValueError('Sequences with diferents size')
-    for index, letter in enumerate(sequencia_1):
-        if letter != sequencia_2[index]:
+    for letter_seq_1, letter_seq_2 in  zip(sequencia_1, sequencia_2):
+        if letter_seq_1 != letter_seq_2:
             contador = contador + 1
     return contador
